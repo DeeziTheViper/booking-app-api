f14f1ce175979f3bcffd4812ac3aaa11b0c8c29b
deezitheviper <amuhdavid@gmail.com>
2022-07-30 19:58:27 +0100

    hotel naming

---
+++ routes/hotels.js
@@ -0,0 +1,39 @@
+import express from "express";
+import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
+import {updateHotel,createHotel,deleteHotel, getHotels } from "../controllers/hotelController.js";
+import { getHotel } from "../controllers/hotelController.js";
+
+
+
+
+const router = express.Router()
+
+
+//get all
+router.get('/', getHotels)
+//get
+router.get('/:id', getHotel)
+//create
+router.post("/",verifyAdmin, createHotel)
+//update
+router.put('/:id',verifyAdmin, updateHotel)
+//Delete
+router.delete('/:id',verifyAdmin, deleteHotel)
+
+router.get('/count', getHotelCount)
+
+
+//another way of handling async calls
+/*
+router.post("/", async (req, res) => {
+    const newHotel = new Hotel(req.body)
+
+    try{
+        const savedHotel = await newHotel.save() 
+        res.status(200).json(savedHotel)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})*/
+
+export default router
